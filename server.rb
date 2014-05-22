5fcf7ee402005ceb3250e6bf1194368f1edf03e3
Rachael Serur <rachaelse@gmail.com>
2014-05-22 11:52:40 -0400

    Index now reads from CSV and inserts intol article list.

--- server.rb
+++ server.rb
@@ -1,1 +1,31 @@
 require 'sinatra'
+require 'csv'
+
+def get_news
+
+  news = []
+
+  CSV.foreach('news.csv', headers: true, header_converters: :symbol) do |row|
+    news << row.to_hash
+  end
+
+  news
+
+end
+
+
+get '/' do
+
+  @news = get_news
+
+  erb :index
+
+end
+
+get '/x' do
+
+  erb :submit
+
+end
+
+
